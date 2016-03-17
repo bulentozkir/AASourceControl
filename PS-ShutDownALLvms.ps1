7d7320c06db36c17328fdc42e2dfbadc2de4174a
bulentozkir <bulentozkir@hotmail.com>
2016-03-17 17:48:45 +0200

    Updated PS-ShutDownALLvms.ps1 in Azure Automation.

--- PS-ShutDownALLvms.ps1
+++ PS-ShutDownALLvms.ps1
@@ -3,4 +3,5 @@ $cred
 $subscription = Get-AutomationVariable -Name 'ContosoSubscriptionID'
 $subscription 
 Login-AzureRMAccount -Credential $cred -SubscriptionId $subscription
-Get-AzureRMVM -ResourceGroupName PremierWorkshop | Stop-AzureRMVm -Force
+Get-AzureRMVM -ResourceGroupName Contoso | Stop-AzureRMVm -Force
+Get-AzureRMVM -ResourceGroupName ContosoDSC | Stop-AzureRMVm -Force
